af995a9a01047480526005e4e60c1dfd93bc18fd
Zabedfolio <zabedfolio@gmail.com>
2026-02-20 01:42:32 +0600

    addd

---
+++ Module 23/Js/handler.js
@@ -0,0 +1,18 @@
+// console.log('ami achi')
+
+ document.getElementById('btn-update').addEventListener('click', function(){
+            // console.log('btnclicked')
+            const titleText = document.getElementById('title-text');
+            titleText.innerText = 'Updated page title'
+        })
+
+ document.getElementById('btn-login').addEventListener('click', function(){
+        // console.log('ji')
+        const userinfo = document.getElementById('user-info');
+        userinfo.innerText = 'User Logged in'
+       })
+
+
+        document.getElementById('btn-update').addEventListener('click', function(){
+        console.log('clicked');
+        })
